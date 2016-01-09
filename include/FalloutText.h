ed5aca4b7b9eaa6a7515558bc0e3a0d28e1e5629
kiradess <tanikaze2501@gmail.com>
2016-01-09 02:12:14 -0500

    update FalloutText

--- include/FalloutText.h
+++ include/FalloutText.h
@@ -3,6 +3,11 @@
 
 #include <map>
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <iomanip>
+#include <sstream>
+#include <unordered_set>
 
 class FalloutText
 {
@@ -12,12 +17,47 @@ private:
             {"logo", "ROBCO INDUSTRIES (TM) TERMLINK PROTOCOL"},
             {"command", "ENTER PASSWORD NOW"}
             };
-            
-    int row_start;
-    int left_col_start;
-    int right_col_start;
-    int line_width;
-    int num_rows;
+    
+    // text up-down bounds (screen coords)        
+    int row_start_;
+    int row_end_;
+    
+    int left_col_start_;
+    int right_col_start_;
+    int line_width_;
+    int num_rows_;
+    
+    // left text column bounds (screen coords)
+    int left_lower_;
+    int left_upper_;
+    
+    // right column bounds (screen coords)
+    int right_lower_;
+    int right_upper_;
+    
+    // random starting memory address
+    int start_address_;
+    
+    // list of hex memory addresses
+    std::vector < std::string > address_list_;
+    
+    // original text strings for searching through
+    std::string left_text_;
+    std::string right_text_;
+    
+    // array for storing right side text strings
+    std::vector < std::string > right_text_list_;
+    int right_max_lines_ = 20;
+    int max_bracket_len_ = 20;
+    
+    std::unordered_set < char > alphabet_ = {
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
+            'R','S','T','U','V','W','X','Y','Z'};
+    std::unordered_set < char > junk_ = {
+            '$','.','\'',':','_','!','\\','"','+','-','%','/','|','*','@','#',
+            '`','=','^',';','?',','};
+    std::vector < char > left_brackets_ = {'(', '[', '<', '{'};
+    std::vector < char > right_brackets_ = {')', ']', '>', '}'};
     
     FalloutText() { } // private default constructor
     
@@ -25,7 +65,9 @@ public:
     FalloutText(int row_start, int left_col_start, int right_col_start, 
             int line_width, int num_rows);
     void report_values(void);
+    int set_start_address(void);
+    void gen_address_list(void);
+    std::string int_address_to_hex_string(int address);
 };
 
 #endif
-
--- src/FalloutText.cpp
+++ src/FalloutText.cpp
@@ -4,19 +4,74 @@
 FalloutText::FalloutText(int row_start, int left_col_start, int right_col_start, 
         int line_width, int num_rows)
 {
-    this->row_start = row_start;
-    this->left_col_start = left_col_start;
-    this->right_col_start = right_col_start;
-    this->line_width = line_width;
-    this->num_rows = num_rows;
+    row_start_ = row_start;
+    row_end_ = row_start + num_rows;
+    
+    left_col_start_ = left_col_start;
+    right_col_start_ = right_col_start;
+    
+    line_width_ = line_width;
+    num_rows_ = num_rows;
+    
+    // left text column bounds (screen coords)
+    left_lower_ = left_col_start;
+    left_upper_ = left_lower_ + line_width - 1;
+    
+    // right column bounds (screen coords)
+    right_lower_ = right_col_start;
+    right_upper_ = right_lower_ + line_width - 1;
+    
+    // pick a random starting memory address
+    start_address_ = set_start_address();
+    
+    // generate a list of addresses starting from start address
+    gen_address_list();
 }
 
 void FalloutText::report_values(void)
 {
-    std::cout << this->row_start << std::endl;
-    std::cout << this->left_col_start << std::endl;
-    std::cout << this->right_col_start << std::endl;
-    std::cout << this->line_width << std::endl;
-    std::cout << this->num_rows << std::endl;
+    std::cout << row_start_ << std::endl;
+    std::cout << left_col_start_ << std::endl;
+    std::cout << right_col_start_ << std::endl;
+    std::cout << line_width_ << std::endl;
+    std::cout << num_rows_ << std::endl;
     std::cout << top_text["logo"] << std::endl;
+}
+
+int FalloutText::set_start_address(void)
+{
+    // maximum memory address
+    int start_address_max = 65535 - (line_width_ * num_rows_ * 2);
+        
+    // select random address between 0x0000 and max
+    return rand() % start_address_max;
+}
+
+void FalloutText::gen_address_list(void)
+{
+    int start = start_address_;
+    
+    while (address_list_.size() < (num_rows_ * 2))
+    {
+        address_list_.push_back(int_address_to_hex_string(start));
+        
+        start += line_width_;
+    }
+}
+
+std::string FalloutText::int_address_to_hex_string(int address)
+{
+    std::stringstream stream;
+    
+    stream << std::hex << address;
+    
+    std::string out = "0x" + stream.str();
+    
+    // convert to uppercase
+    for (auto & c: out) 
+    {
+        c = toupper(c);
+    }
+    
+    return out;
 }
